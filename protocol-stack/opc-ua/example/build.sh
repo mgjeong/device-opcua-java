14cec4f9248024fa293c56e43997312d5fc409ca
ChaJiWon <jw_wonny.cha@samsung.com>
2017-11-13 20:58:09 +0900

    Modify build.sh of OPC-UA sample application

--- protocol-stack/opc-ua/example/build.sh
+++ protocol-stack/opc-ua/example/build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-opcua_home="../edge-opcua/mavenProjects"
+build_home=$(pwd)
+cd ../edge-opcua
 client_home="opcua-client"
 server_home="opcua-server"
 build_cmd="mvn clean compile assembly:single"
 
-cd $opcua_home
 $build_cmd
 
 mvn install:install-file -Dfile=./target/opcua-adapter-0.0.1-SNAPSHOT-jar-with-dependencies.jar -DgroupId=com.edge.compute1.protocol -DartifactId=opcua-adapter -Dversion=0.0.1-SNAPSHOT-jar-with-dependencies -Dpackaging=jar -DgeneratePom=true
 
-cd -
+cd $build_home
 cd $client_home
 $build_cmd
 cp ./target/opcua-client-0.0.1-SNAPSHOT-jar-with-dependencies.jar ../target
