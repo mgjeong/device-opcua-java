e95f35419b86122cca3d7203344aa7a8ba09d352
jihwan.seo <jihwan.seo@samsung.com>
2017-11-22 10:33:30 +0900

    apply new dependency related protocol-stack

--- dependencys/build.sh
+++ dependencys/build.sh
@@ -6,4 +6,10 @@ cd $DEP_ROOT
 git clone git@github.sec.samsung.net:RS7-EdgeComputing/EMF.git
 cd EMF/java/edgex-emf
 ./build.sh
+
+cd $DEP_ROOT
+rm -rf protocol-opcua
+git clone git@github.sec.samsung.net:RS7-EdgeComputing/protocol-opcua.git
+cd protocol-opcua/java/edge-opcua
+./build.sh
 echo "done"
