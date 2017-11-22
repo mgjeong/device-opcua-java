8057ae1114c3830420cd60235a55290a85b6f27b
차지원/IoT Lab(S/W센터)/Engineer/삼성전자 <jw_wonny.cha@samsung.com>
2017-11-22 19:22:35 +0900

    Update build.sh

--- dependencys/build.sh
+++ dependencys/build.sh
@@ -3,13 +3,8 @@ DEP_ROOT=$(pwd)
 
 #start clone dependencys git repo and maven install
 cd $DEP_ROOT
-git clone git@github.sec.samsung.net:RS7-EdgeComputing/messaging-zmq.git
+git clone git@github.com:mgjeong/messaging-zmq.git
 cd messaging-zmq/java/edgex-emf
 ./build.sh
 
-#cd $DEP_ROOT
-#rm -rf protocol-opcua
-#git clone git@github.sec.samsung.net:RS7-EdgeComputing/protocol-#opcua.git
-#cd protocol-opcua/java/edge-opcua
-#./build.sh
 echo "done"
