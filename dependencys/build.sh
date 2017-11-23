71bf106ab36019b0b009d61cf4aa53412671583a
차지원/IoT Lab(S/W센터)/Engineer/삼성전자 <jw_wonny.cha@samsung.com>
2017-11-23 18:20:06 +0900

    Update build.sh

--- dependencys/build.sh
+++ dependencys/build.sh
@@ -3,7 +3,7 @@ DEP_ROOT=$(pwd)
 
 #start clone dependencys git repo and maven install
 cd $DEP_ROOT
-git clone git@github.com:mgjeong/messaging-zmq.git
+git clone https://github.com/mgjeong/messaging-zmq.git
 cd messaging-zmq/java/edgex-emf
 ./build.sh
 
