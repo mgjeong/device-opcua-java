f905560cf738bd3c36a1b5ff82a8623b6b62c01e
jihwan.seo <jihwan.seo@samsung.com>
2017-11-28 13:26:35 +0900

    disable command dependency

--- dependencys/build.sh
+++ dependencys/build.sh
@@ -7,9 +7,9 @@ git clone https://github.com/mgjeong/messaging-zmq.git
 cd messaging-zmq/java/edgex-emf
 ./build.sh
 
-cd $DEP_ROOT
-git clone https://github.sec.samsung.net/RS7-EdgeComputing/command-json-format-java.git
-cd command-json-format-java/java/edge-json-format
-./build.sh
+#cd $DEP_ROOT
+#git clone https://github.sec.samsung.net/RS7-EdgeComputing/command-#json-format-java.git
+#cd command-json-format-java/java/edge-json-format
+#./build.sh
 
 echo "done"
