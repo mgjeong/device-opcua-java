d53b471577ccedb82bdbcbbfa3b5ea71fcbe114c
jihwan.seo <jihwan.seo@samsung.com>
2017-11-30 19:44:24 +0900

    move src folder to root path

--- dependencies/build.sh
+++ dependencies/build.sh
@@ -8,6 +8,11 @@ cd messaging-zmq/java/edgex-emf
 ./build.sh
 
 cd $DEP_ROOT
+git clone https://github.sec.samsung.net/RS7-EdgeComputing/protocol-opcua-java.git
+cd protocol-opcua-java/java/edge-opcua
+./build.sh
+
+cd $DEP_ROOT
 git clone https://github.sec.samsung.net/RS7-EdgeComputing/datamodel-command-java.git
 cd datamodel-command-java/command-json-format
 ./build.sh
