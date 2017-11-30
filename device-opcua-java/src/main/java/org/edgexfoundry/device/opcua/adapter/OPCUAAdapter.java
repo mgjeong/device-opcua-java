e354a19954565932a8227584875d2df7dccb6b4e
jihwan.seo <jihwan.seo@samsung.com>
2017-11-30 12:57:12 +0900

    apply copyright and remove sample metadata

--- device-opcua-java/src/main/java/org/edgexfoundry/device/opcua/adapter/OPCUAAdapter.java
+++ device-opcua-java/src/main/java/org/edgexfoundry/device/opcua/adapter/OPCUAAdapter.java
@@ -1,4 +1,22 @@
-package org.edgexfoundry.device.opcua.core;
+/******************************************************************
+ *
+ * Copyright 2017 Samsung Electronics All Rights Reserved.
+ *
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ ******************************************************************/
+
+package org.edgexfoundry.device.opcua.adapter;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -23,7 +41,7 @@ import org.edge.protocol.opcua.api.common.EdgeOpcUaCommon;
 import org.edge.protocol.opcua.api.common.EdgeRequest;
 import org.edge.protocol.opcua.api.common.EdgeStatusCode;
 import org.edgexfoundry.device.opcua.DataDefaultValue;
-import org.edgexfoundry.device.opcua.emf.EMFAdapter;
+import org.edgexfoundry.device.opcua.adapter.emf.EMFAdapter;
 import org.edgexfoundry.device.opcua.metadata.MetaDataType;
 import org.edgexfoundry.device.opcua.opcua.OPCUADriver.DriverCallback;
 import org.edgexfoundry.domain.meta.Addressable;
