5535d83aac381a1d4f112ee9fc0485dd88f321bd
jeongin-kim <jeongin.kim@samsung.com>
2017-12-14 16:42:12 +0900

    Add RESULT in OPCUAMessageKeyIdentifier - Add RESULT in OPCUAMessageKeyIdentifier

--- src/main/java/org/edgexfoundry/device/opcua/adapter/OPCUAMessageKeyIdentifier.java
+++ src/main/java/org/edgexfoundry/device/opcua/adapter/OPCUAMessageKeyIdentifier.java
@@ -3,15 +3,16 @@ package org.edgexfoundry.device.opcua.adapter;
 public enum OPCUAMessageKeyIdentifier {
   OPERATION(0, "operation", "command operation"),
   INPUT_ARGUMENT(1, "input_argument", "input value in put command operation"),
-  VALUE(2, "value", "command result value"),
-  VALUE_DESCRIPTOR(3, "value_descriptor", "command valuedescriptor name"),
-  SAMPLING_INTERVAL(4, "sampling_interval", "sampling interval"), 
-  APPLICATION_NAME(5, "application_name", "application name for server"),
-  APPLICATION_URI(6, "application_uri", "application uri for server"),
-  SECURITYPOLICY_URI(7, "security_policy_uri", "security policy uri for server"),
-  ENDPOINT_URI(8, "endpoint_uri", "endpoint uri for server"), 
-  ENDPOINT_INFOMATION(9, "endpoint_information", "endpoint infomation for server"), 
-  RESPONSE_INFO(10,"response_information", "response infomation");
+  RESULT(2, "result", "command result status"),
+  VALUE(3, "value", "command result value"),
+  VALUE_DESCRIPTOR(4, "value_descriptor", "command valuedescriptor name"),
+  SAMPLING_INTERVAL(5, "sampling_interval", "sampling interval"), 
+  APPLICATION_NAME(6, "application_name", "application name for server"),
+  APPLICATION_URI(7, "application_uri", "application uri for server"),
+  SECURITYPOLICY_URI(8, "security_policy_uri", "security policy uri for server"),
+  ENDPOINT_URI(9, "endpoint_uri", "endpoint uri for server"), 
+  ENDPOINT_INFOMATION(10, "endpoint_information", "endpoint infomation for server"), 
+  RESPONSE_INFO(11,"response_information", "response infomation");
 
   private int code;
   private String value;
