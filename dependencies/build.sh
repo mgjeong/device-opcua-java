6c8854bd9043a186f646c7160553227a6d86b257
ChaJiWon <jw_wonny.cha@samsung.com>
2017-12-22 16:26:46 +0900

    Modfify repository address to public in build.sh of dependency

--- dependencies/build.sh
+++ dependencies/build.sh
@@ -3,8 +3,8 @@ DEP_ROOT=$(pwd)
 
 #start clone dependencies git repo and maven install
 cd $DEP_ROOT
-#git clone https://github.com/mgjeong/messaging-zmq.git
-git clone git@github.sec.samsung.net:RS7-EdgeComputing/protocol-ezmq-java.git
+git clone https://github.com/mgjeong/protocol-ezmq-java.git
+#git clone git@github.sec.samsung.net:RS7-EdgeComputing/protocol-ezmq-java.git
 if [ ! -d "protocol-ezmq-java" ]
 then
     echo "build fail"
@@ -15,8 +15,8 @@ else
 fi
 
 cd $DEP_ROOT
-#git clone https://github.sec.samsung.net/RS7-EdgeComputing/protocol-opcua-java.git
-git clone git@github.sec.samsung.net:RS7-EdgeComputing/protocol-opcua-java.git
+git clone https://github.com/mgjeong/protocol-opcua-java.git
+#git clone git@github.sec.samsung.net:RS7-EdgeComputing/protocol-opcua-java.git
 if [ ! -d "protocol-opcua-java" ]
 then
     echo "build fail"
@@ -27,8 +27,8 @@ else
 fi
 
 cd $DEP_ROOT
-#git clone https://github.sec.samsung.net/RS7-EdgeComputing/datamodel-command-java.git
-git clone git@github.sec.samsung.net:RS7-EdgeComputing/datamodel-command-java.git
+git clone https://github.com/mgjeong/datamodel-command-java.git
+#git clone git@github.sec.samsung.net:RS7-EdgeComputing/datamodel-command-java.git
 if [ ! -d "datamodel-command-java" ]
 then
     echo "build fail"
